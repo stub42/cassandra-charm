20516f2210044435d072fce1b4b7de049d192a79
David Ames <david.ames@canonical.com>
2014-06-16 16:08:37 -0700

    Functioning install hook

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -5,6 +5,9 @@ import os
 import os.path
 import shutil
 import socket
+import glob
+import subprocess
+import shutil
 
 import _pythonpath
 _ = _pythonpath
@@ -12,10 +15,14 @@ _ = _pythonpath
 from charmhelpers.contrib.templating.pyformat import render
 from charmhelpers import fetch
 
-from charmhelpers.core.hookenv import hookenv, host
+from charmhelpers.core import hookenv, host
 from charmhelpers.contrib.charmsupport import nrpe
 
+from charmhelpers.payload.archive import extract
+
 hooks = hookenv.Hooks()
+
+# XXX juju logging
 
 
 def Template(*args, **kw):
@@ -29,7 +36,7 @@ def Template(*args, **kw):
 
 
 def _get_cassandra_config_dir():
-    if hookenv.config('datastax'):
+    if hookenv.config('dse'):
         return '/etc/dse/cassandra/'
     else: 
         return '/etc/cassandra/'
@@ -64,6 +71,8 @@ def cassandra_yaml_template(config_data=None):
 
     # XXX Add num_tokens vs initial token check
     # XXX Handle initial tokens if needed
+    # XXX DSE vs cassandra and different versions
+    # i.e. exception=Cannot create property=commit_failure_policy 
 
     # This bit of insanity sends the full dictionary as a dict member
     # to overcome the use of VAR-NAME rather than VAR_NAME in
@@ -96,6 +105,7 @@ def cassandra_env_template(config_data=None):
     contents = Template(open(template_file).read()).render(config_data)
     host.write_file(_get_cassandra_env_file(), contents )
 
+
 def cassandra_rackdc_template(config_data=None):
     if config_data is None:
         config_data = hookenv.config()
@@ -122,13 +132,72 @@ def update_nrpe_checks():
         )
     nrpe_compat.write()
 
+def install_dse():
+    java_jna_jar = os.path.join("/usr", "share", "java", "jna.jar")
+    
+    # Install prerequisites
+    packages = ['default-jre-headless', 'libcommons-daemon-java', 'libjna-java',
+                'python-support']
+    fetch.apt_install(packages, fatal=True)
+
+    # Setup Oracle Java
+    # XXX Install from package?
+    if hookenv.config('private_jre_url'):
+        source = fetch.install_remote(hookenv.config('private_jre_url'))
+        shutil.move(" ".join(glob.glob(os.path.join(source, "*"))),
+                    os.path.join("/usr", "lib", "jvm", "oracle-jre"))
+
+        subprocess.check_call(["update-alternatives", "--install", "/usr/bin/java",
+                               "java", "/usr/lib/jvm/oracle-jre/bin/java", "1"])
+        subprocess.check_call(["update-alternatives", "--set", "java",
+                               "/usr/lib/jvm/oracle-jre/bin/java"])
+
+    # Use modern JNA
+    if hookenv.config('private_jna_url'):
+        source = fetch.install_remote(hookenv.config('private_jna_url'))
+        shutil.move(os.path.join(source, "jna"), os.path.join("/usr", "share", "java"))
+        subprocess.check_call(["rm", "-f", java_jna_jar])
+        host.symlink(os.path.join("/usr", "share", "java", "jna", "dist", "jna.jar"), java_jna_jar)
+
+    # Install DSE
+    if hookenv.config('private_dse_url'):
+        source = fetch.install_remote(hookenv.config('private_dse_url'))
+        cmd = ['dpkg', "--install" ] + glob.glob(os.path.join(source, "dse", "*"))
+        subprocess.check_call(cmd)
+    else:
+        packages = ['dse-full']
+        fetch.apt_install(packages, fatal=True)
+
 
 @hooks.hook()
 def install():
-    packages = ['rsync', 'python-jinja2', 'procmail']
+    # Pre-exec
+    for f in glob.glob('exec.d/*/charm-pre-install'):
+        if os.path.isfile(f) and os.access(f, os.X_OK):
+            subprocess.check_call(['sh', '-c', f])
+
+    packages = ['python-jinja2']
     fetch.apt_install(packages, fatal=True)
+    
+    if hookenv.config('apt-repo-spec'):
+        fetch.add_source(hookenv.config('apt-repo-spec'),key=hookenv.config('apt-repo-key'))
 
+    fetch.apt_update(fatal=True)
 
+    if hookenv.config('extra_packages'):
+        packages = [hookenv.config('extra_packages')]
+        fetch.apt_install(packages, fatal=True)
+
+    if hookenv.config('dse'):
+        install_dse() 
+    else:
+        # XXX disable cassandra start?
+        packages = ['cassandra']
+        fetch.apt_install(packages, fatal=True)
+    
+
+# XXX restart cassandra? bash used bzr 
+# XXX wait for cassandra up
 
 @hooks.hook('config-changed','upgrade-charm')
 def config_changed():
