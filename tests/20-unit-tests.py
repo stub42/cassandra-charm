82dca942965a0deedf8c145b068bddf3f539e2b4
Stuart Bishop <stuart@stuartbishop.net>
2014-11-21 21:52:00 +0700

    actions.configure_sources tests

--- tests/20-unit-tests.py
+++ tests/20-unit-tests.py
@@ -9,12 +9,25 @@ from unittest.mock import patch
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.pardir, 'hooks')))
 
+from charmhelpers.core import hookenv
+import charmhelpers.fetch
+
 import actions
 import helpers
 
 
 @patch('helpers.is_lxc', lambda: False)
 class TestsActions(unittest.TestCase):
+
+    def setUp(self):
+        # Mock charm environment.
+        charm_dir = tempfile.TemporaryDirectory()
+        self.addCleanup(charm_dir.cleanup)
+        mock_env = patch.dict(os.environ, dict(CHARM_DIR=charm_dir.name))
+        mock_env.start()
+        self.addCleanup(mock_env.stop)
+
+
     @patch('subprocess.check_call')
     def test_swapoff(self, check_call):
         fstab = (
@@ -28,6 +41,45 @@ class TestsActions(unittest.TestCase):
             self.assertTrue(b'swap' not in f.read())
 
         check_call.assert_called_once_with(['swapoff', '-a'])
+
+    @patch('charmhelpers.fetch.configure_sources')
+    @patch('charmhelpers.core.hookenv.config')
+    def test_configure_sources(self, config, configure_sources):
+        config.return_value = hookenv.Config()
+
+        def reload_config():
+            config().save()
+            config.return_value = hookenv.Config()
+
+        # fetch.configure_sources called the first time
+        actions.configure_sources('')
+        configure_sources.assert_called_once_with(True)
+
+        def reload_config():
+            config().save()
+            config.return_value = hookenv.Config()
+
+        # fetch.configure_sources not called if relevant config is unchanged.
+        reload_config()
+        configure_sources.reset_mock()
+        actions.configure_sources('')
+        self.assertFalse(configure_sources.called)
+
+        # Changing install_sources causes fetch.configure_sources to be
+        # called.
+        reload_config()
+        configure_sources.reset_mock()
+        config()['install_sources'] = 'foo'
+        actions.configure_sources('')
+        configure_sources.assert_called_once_with(True)
+
+        # Changing install_keys causes fetch.configure_sources to be
+        # called.
+        reload_config()
+        configure_sources.reset_mock()
+        config()['install_keys'] = 'foo'
+        actions.configure_sources('')
+        configure_sources.assert_called_once_with(True)
 
 
 if __name__ == '__main__':
