728343484d4c19e09dc56b1745c8669ba5b3dbc0
Stuart Bishop <stuart@stuartbishop.net>
2014-11-25 14:49:27 +0700

    sync

---
+++ hooks/charmhelpers/__init__.py
@@ -0,0 +1,13 @@
+# Bootstrap charm-helpers, installing its dependencies if necessary using
+# only standard libraries.
+try:
+    import six
+except ImportError:
+    import subprocess
+    import sys
+    if sys.version_info.major == 2:
+        subprocess.check_call(['apt-get', 'install', '-y', 'python-six'])
+    else:
+        subprocess.check_call(['apt-get', 'install', '-y', 'python3-six'])
+    import six
+_ = six
